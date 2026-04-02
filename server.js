4f14d0f3cbcbce8070bdb096a83a23cc8a32fa5d
Vinicius Odorizzi Hoppe <viniciusodorizzi25@gmail.com>
2026-04-02 15:57:23 -0300

    port do koyeb

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./src/app');
 const connectDB = require('./src/config/database');
 const logger = require('./src/config/logger');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 // Debug imediato
 console.log('🚀 Starting server...');
 
